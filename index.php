249f55549fa2a0dc3ded81de8e25a9c9d447d5f7
octador <mutuon.j@gmail.com>
2023-12-13 14:44:12 +0100

    exo 3 PHP_PDO

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<h1>exercice 1</h1>
 <?php
 
 $dsn = 'mysql:host=localhost;dbname=colyseum';
@@ -16,3 +17,30 @@ foreach ($users as $user)  {
 }
 
 ?>
+<!-- ------------------exo 2-------------------------------------------- -->
+<h1>exercice 2</h1>
+
+<?php
+
+$request = $db->query('SELECT * FROM showtypes');
+$showtypes = $request->fetchAll();
+
+foreach ($showtypes as $showtype) {
+    echo '<p>' . $showtype['type'] . '</p>';
+}
+?>
+<!-- ----------------------exo 3---------------------------------------- -->
+<h1>exercice 3</h1>
+<?php
+
+$request = $db->query('SELECT * FROM clients LIMIT 20');
+$users20tops = $request->fetchAll();
+
+var_dump($users20tops);
+
+foreach($users20tops as $users20top){
+    echo '<p>'.$users20top['lastName'] .'</p>';
+}
+;
+?>
+<!-- ----------------------------exo 4------------------------ -->
